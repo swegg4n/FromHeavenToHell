f2b83bf9553fc451ba0146ed214643e2ad92bafb
Filip Moreau <filip.moreau@hotmail.com>
2019-03-28 17:09:42 +0100

    Skapade ett ramverk för abilities

---
+++ FromHeavenToHell/Assets/Scripts/AbilityScripts/AbilityProperty.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Ability", menuName = "Abilities/New Ability", order = 1)]
+public class AbilityProperty : ScriptableObject
+{
+    public enum AbilityType { Projectile, Ray };
+
+    public Texture2D icon;
+    public Color color;
+
+    public AbilityType abilityType;
+
+    public float
+        damage,
+        speed,
+        fireRate,
+        castTime;
+
+    public GameObject prefab;
+
+
+
+    public void UseAbility(Vector3 position, Vector3 direction)
+    {
+        Debug.Log("fire");
+
+        var projectile = Instantiate(prefab, position, Quaternion.identity);
+
+        projectile.GetComponent<SpriteRenderer>().color = color;
+
+        projectile.GetComponent<Rigidbody2D>().velocity = direction.normalized * speed;
+
+    }
+
+}
--- FromHeavenToHell/Assets/Scripts/AbilityScripts/Fire.cs
+++ FromHeavenToHell/Assets/Scripts/AbilityScripts/Fire.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    [SerializeField] private GameObject projectile;
-
-    [SerializeField] private bool player1;
-
+    [SerializeField] private AbilityProperty selectedAbility;
+    
+    [SerializeField] private bool isPlayer1;
+    /*
     private bool fireInput;
 
     private float projectileSpeed = 1f;
+    */
 
     private void Update()
     {
-        if(player1 == true)
+        if (Input.GetButtonDown("R1P1") == true && isPlayer1 == true)
+        {
+            if (selectedAbility != null)
+                selectedAbility.UseAbility(transform.position, GetComponentInChildren<AimIndicator>().direction);
+        }
+
+        /*
+        if (player1 == true)
         {
             FireShot("R1P1");
         }
-        else if(player1 == false)
+        else if (player1 == false)
         {
             FireShot("R1P2");
         }
@@ -29,17 +37,16 @@ public class Fire : MonoBehaviour
             //    var bullet = Instantiate(projectile, transform.position, Quaternion.identity);
             //    bullet.GetComponent<Rigidbody2D>().velocity = GetComponentInChildren<AimIndicator>().direction.normalized * projectileSpeed;
             //}
-        }
+        }*/
     }
 
-    private void FireShot(string input)
-    {
-        fireInput = Input.GetButtonDown(input);
-
+    //private void Fire()
+    //{
+        /*
         if (fireInput == true)
         {
             var bullet = Instantiate(projectile, transform.position, Quaternion.identity);
             bullet.GetComponent<Rigidbody2D>().velocity = GetComponentInChildren<AimIndicator>().direction.normalized * projectileSpeed;
-        }
-    }
+        }*/
+    //}
 }
