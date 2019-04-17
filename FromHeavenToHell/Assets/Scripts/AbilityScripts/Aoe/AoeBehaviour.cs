095ec9959f393b6ba2ce4497e5657042a5c2ce55
Jonathan Michaeli <jonathan.p.michaeli@gmail.com>
2019-04-17 14:05:53 +0200

    Fixat bug med AOE där den bara skadade en åt gågnen
    Co-Authored-By: oporenius <oporenius@users.noreply.github.com>

--- FromHeavenToHell/Assets/Scripts/AbilityScripts/Aoe/AoeBehaviour.cs
+++ FromHeavenToHell/Assets/Scripts/AbilityScripts/Aoe/AoeBehaviour.cs
@@ -7,6 +7,7 @@ public class AoeBehaviour : MonoBehaviour
     public AoeBoxAbility aoeAbility { set; get; }
     public GameObject Caster { set; get; }
     private float timeSinceLastTick;
+    private bool resetClock;
     [SerializeField] private bool selfDamage;
 
     void Start()
@@ -16,30 +17,32 @@ public class AoeBehaviour : MonoBehaviour
 
     void Update()
     {
+        if(resetClock == true)
+        {
+            timeSinceLastTick = 0;
+            resetClock = false;
+        }
+
         timeSinceLastTick += Time.deltaTime;
     }
 
     protected virtual void OnTriggerStay2D(Collider2D other)
     {
-        Debug.Log("TriggerStay");
         if(timeSinceLastTick > aoeAbility.GetTimeBetweenTicks())
         {
             if (Caster.tag != other.tag || selfDamage == true || Caster == null)
             {
-                Debug.Log("1");
-
                 if (other.tag == "Enemy")
                 {
-                    Debug.Log("2");
+                    Debug.Log("Hit" + timeSinceLastTick);
                     other.GetComponent<EnemyBaseClass>().TakeDamage(aoeAbility.Damage);
                 }
                 else if (other.tag == "PlayerAngel" || other.tag == "PlayerDemon")
                 {
-                    Debug.Log("3");
                     PlayerManager.instance.TakeDamage(aoeAbility.Damage);
                 }
             }
-            timeSinceLastTick = 0;
+            resetClock = true;
         }
     }
 }
