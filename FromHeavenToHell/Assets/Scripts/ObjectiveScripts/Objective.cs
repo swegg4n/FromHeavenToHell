21a553bb0f2a549d5cfc2876a7a1c27e51d9b5a5
swegg4n <filip.moreau@hotmail.com>
2019-04-24 22:10:22 +0200

    Objectives och objective-kontroller

---
+++ FromHeavenToHell/Assets/Scripts/ObjectiveScripts/Objective.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Objective/Multi-Objective")]
+public class Objective : ScriptableObject
+{
+    [SerializeField] bool isKillObjective;      //om detta är ett döda-objektiv
+    public bool IsKillObjective { get { return isKillObjective; } }
+
+    [SerializeField] bool isSurviveObjective;       //om detta är ett överlevnads-objektiv
+    public bool IsSurviveObjective { get { return isSurviveObjective; } }
+
+
+    [Tooltip("The amount of enemies required to kill to complete the objective")]
+    [SerializeField] private int killCount;       //Antal fiender som måster dödas
+    public int KillCount { get { return killCount; } }
+
+    [Tooltip("The number of seconds the players need to survive before completing the objective")]
+    [SerializeField] private float survivalTime;        //Tiden man måste överleva. Mäts i sekunder
+    public float SurvivalTime { get { return survivalTime; } }
+}
---
+++ FromHeavenToHell/Assets/Scripts/ObjectiveScripts/ObjectiveController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ObjectiveController : MonoBehaviour
+{
+    [SerializeField] private Objective objective;//temp serializeField, ska fås från rummet (currentRoom.objective  typ)
+    private bool objectiveCompleted;
+
+    private int killCount;
+    private float timePassed;
+
+
+    /// <summary>
+    /// Resettar objektivet
+    /// Ska kallas vid varje rum-byte
+    /// </summary>
+    private void StartObjective()
+    {
+        /*set objective from room*/
+        objectiveCompleted = false;
+        killCount = 0;
+        timePassed = 0;
+
+        ValidateCompletion();
+    }
+
+    /// <summary>
+    /// Timer som räknar upp tills spelarna överlevt tillräckligt länge 
+    /// </summary>
+    private void Update()
+    {
+        if (objective.IsSurviveObjective == true && objectiveCompleted == false)
+        {
+            if (timePassed < objective.SurvivalTime)
+            {
+                timePassed += Time.deltaTime;
+                Debug.Log(timePassed);
+            }
+            else
+            {
+                ValidateCompletion();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Kontrollerar om alla del-objektiv har utfötrs
+    /// </summary>
+    private void ValidateCompletion()
+    {
+        bool killCompleted;
+        bool survivalCompleted;
+
+        if (objective.IsKillObjective == false || killCount >= objective.KillCount)  //kontrollerar om objektivet är ett döda-objektiv och om spelarna isf. har dödat tillräckligt många fiender
+        {
+            killCompleted = true;
+        }
+        else
+        {
+            killCompleted = false;
+        }
+
+        if (objective.IsSurviveObjective == false || timePassed >= objective.SurvivalTime)   //kontrollerar om objektivet är ett överlevnads-objektiv och om spelarna isf. har överlevt tillräckligt länge
+        {
+            survivalCompleted = true;
+        }
+        else
+        {
+            survivalCompleted = false;
+        }
+
+
+        if (killCompleted == true && survivalCompleted == true)
+        {
+            objectiveCompleted = true;
+            Debug.Log("OBJECTIVE COMPLETED!");
+        }
+    }
+
+}
